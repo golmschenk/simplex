ab5f4bedd42560d9f9bc998e21cb6246a62e3c9a
Greg Olmschenk <golmschenk@gmail.com>
2015-05-10 20:52:49 -0400

    Simplex: Slack variable adding

--- simplex.py
+++ simplex.py
@@ -3,7 +3,15 @@ The class for solving simplex.
 Given a standard LPP, finds the optimal solution through simplex method.
 """
 
-import numpy
+import numpy as np
 
 class Simplex:
-    pass
+    """Class to preform simplex."""
+    def __init__(self):
+        self.A = np.array([[]])
+        self.b = np.array([[]])
+
+    def add_slack(self):
+        """Adds the slack identity matrix to the A matrix."""
+        identity_size = self.A.shape[0]
+        self.A = np.append(self.A, np.identity(identity_size), axis=1)
--- tests/test_simplex.py
+++ tests/test_simplex.py
@@ -7,14 +7,31 @@ class TestSimplex:
     """Tests for the simplex class."""
     def test_can_add_slack_variables_to_matrix(self):
         """Checks that the slack variables can be added correctly."""
+        simplex = Simplex()
         A = np.array([[1,  1],
                       [1, -1]])
-        expected_A_with_slack = np.array([[1,  1, 1, 0],
-                                          [1, -1, 0, 1]])
+        simplex.A = A
+        expected_a = np.array([[1,  1, 1, 0],
+                               [1, -1, 0, 1]])
 
-        A_with_slack = Simplex.add_slack(A)
+        simplex.add_slack()
 
-        assert A_with_slack == expected_A_with_slack
+        assert np.array_equal(simplex.A, expected_a)
+
+    def test_can_add_slack_variables_for_different_size_matrices(self):
+        """Checks that the slack variables can be added for larger matrices."""
+        simplex = Simplex()
+        A = np.array([[1,  1, 1],
+                      [1, -1, 2],
+                      [2,  1, 1]])
+        simplex.A = A
+        expected_a = np.array([[1,  1, 1, 1, 0, 0],
+                               [1, -1, 2, 0, 1, 0],
+                               [2,  1, 1, 0, 0, 1]])
+
+        simplex.add_slack()
+
+        assert np.array_equal(simplex.A, expected_a)
 
     def test_can_make_tableau_from_constraints(self):
         # A = np.array([[]])
