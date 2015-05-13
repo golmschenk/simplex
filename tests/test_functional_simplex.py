be3b2182a0842461ea0c3f70946daec83c0c9308
Greg Olmschenk <golmschenk@gmail.com>
2015-05-13 17:58:11 -0400

    Simplex: Another full simplex test

--- tests/test_functional_simplex.py
+++ tests/test_functional_simplex.py
@@ -19,3 +19,19 @@ class TestFunctionalSimplex:
         assert simplex.is_optimal
         assert simplex.value == 11
         assert np.array_equal(simplex.solution, np.array([[3], [1]]))
+
+    def test_full_simplex2(self):
+        coefficients = np.array([[ 1,  1],
+                                 [ 3, -8],
+                                 [10,  7]])
+        constraints = np.array([[ 4],
+                                [24],
+                                [35]])
+        objective = np.array([5, 7])
+        simplex = Simplex(coefficients=coefficients, constraints=constraints, objective=objective)
+
+        simplex.run()
+
+        assert simplex.is_optimal
+        assert simplex.value == 28
+        assert np.array_equal(simplex.solution, np.array([[0], [4]]))
